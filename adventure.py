0911f3c5ae34a84be7923faf4c60249071321907
Nik Strauss <nik@localhost.localdomain>
2015-01-19 19:01:19 -0600

    Finished classes

--- adventure.py
+++ adventure.py
@@ -15,7 +15,7 @@ if race == "H":
 	lk = 5 #Luck
 	print ("Hail, Human", name)
 elif race == "E":
-	hp = 	ap = 0
+	hp = 3 	
 	ap = 0
 	st = 4
 	sp = 6
@@ -37,4 +37,17 @@ if cc == "W":
 	hp = hp+2
 	st = st+2
 	sp = sp+1
+	mp = 0
+elif cc == "M":
+	hp = hp-2
+	st = st-3
+	sp = sp-3
+	mp = mp+5
+	lk = lk-2
+elif cc == "R":
+	hp = hp-1
+	st = st-1
+	sp = sp+3
+	mp = mp-2
+	lk = lk+3
 
